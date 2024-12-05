f950d3d772c9438e461e1dc98f88f667911582c3
mmmsalmon <188800011+mmmsalmon@users.noreply.github.com>
2024-12-04 23:48:51 -0500

    day 4

--- 4.py
+++ 4.py
@@ -26,8 +26,30 @@ def check(x: int, y: int) -> int:
     return choice.count(["X", "M", "A", "S"])
 
 
-total = sum(
-    check(x, y) for y, line in enumerate(lines) for x, c in enumerate(line) if c == "X"
-)
+total = sum(check(x, y) for y, line in enumerate(lines) for x, c in enumerate(line) if c == "X")
 
 print(f"part 1: {total}")
+
+
+#############
+def check2(x: int, y: int) -> int:
+    diag, mas = "", 0
+
+    if x + 1 < len(lines[0]) and y + 1 < len(lines):
+        diag += lines[y + 1][x + 1]  # ↘
+    if 0 <= x - 1 and 0 <= y - 1:
+        diag += lines[y - 1][x - 1]  # ↖
+    if 0 <= x - 1 and y + 1 < len(lines):
+        diag += lines[y + 1][x - 1]  # ↙
+    if x + 1 < len(lines[0]) and 0 <= y - 1:
+        diag += lines[y - 1][x + 1]  # ↗
+
+    if diag.count("M") == 2 and diag.count("S") == 2 and diag != "SSMM" and diag != "MMSS":
+        mas = 1
+
+    return mas
+
+
+total = sum(check2(x, y) for y, line in enumerate(lines) for x, c in enumerate(line) if c == "A")
+
+print(f"part 2: {total}")
