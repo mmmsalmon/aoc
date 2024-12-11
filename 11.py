e3343636c8fe6bfec0a6489abea35640055a6d5e
mmmsalmon <188800011+mmmsalmon@users.noreply.github.com>
2024-12-11 01:19:07 -0500

    day 11 - cleanup

--- 11.py
+++ 11.py
@@ -1,17 +1,18 @@
 with open("input/11", "r") as f:
-    line = f.read().split(' ')
+    line = f.read().split(" ")
+
 
 def splitstones(input_):
     stones = []
     for stone in input_:
-        if stone == '0':
-            stones.append('1')
+        if stone == "0":
+            stones.append("1")
         elif len(stone) % 2 == 0:
-            stones.append(stone[:len(stone)//2].lstrip('0'))
-            stones.append(stone[len(stone)//2:].lstrip('0'))
+            stones.append(stone[: len(stone) // 2].lstrip("0"))
+            stones.append(stone[len(stone) // 2 :].lstrip("0"))
         else:
-            stones.append(str(int(stone)*2024))
-    stones = ['0' if i=='' else i for i in stones]
+            stones.append(str(int(stone) * 2024))
+    stones = ["0" if i == "" else i for i in stones]
     return stones
 
 
@@ -20,4 +21,4 @@ for i in range(24):
     stonelist = splitstones(stonelist)
     print(len(stonelist))
 
-print(f'part 1: {len(stonelist)}')
+print(f"part 1: {len(stonelist)}")
